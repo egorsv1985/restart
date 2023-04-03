eafb2926f1f83e52c6b17e91f1ef9ade508cdceb
egorsv1985 <egorvs2009@gmail.com>
2023-04-03 11:58:08 +0300

    added adaptive with header

--- src/js/app.js
+++ src/js/app.js
@@ -222,3 +222,130 @@ window.addEventListener("scroll", () => {
 	lastScroll = currentScroll;
 });
 
+
+const TABLIST = document.querySelector("#tablist");
+const TABS = [...TABLIST.querySelectorAll(".tab")];
+const TABPANELS = [...document.querySelectorAll(".tabpanel")];
+
+const showActivePanel = (element) => {
+  const selectedId = element.id;
+  TABPANELS.forEach((e) => {
+    e.hidden = "true";
+  });
+  const activePanel = document.querySelector(
+    `[aria-labelledby="${selectedId}"]`
+  );
+  activePanel.removeAttribute("hidden");
+};
+
+const setSelectedTab = (element) => {
+  const selectedId = element.id;
+  TABPANELS[0].classList.remove("visible");
+
+  TABS.forEach((e) => {
+    const id = e.getAttribute("id");
+    if (id === selectedId) {
+      e.removeAttribute("tabindex", "0");
+      e.setAttribute("aria-selected", "true");
+    } else {
+      e.setAttribute("tabindex", "-1");
+      e.setAttribute("aria-selected", "false");
+    }
+  });
+};
+
+const createArrowNavigation = () => {
+  const firstTab = TABS[0];
+  const lastTab = TABS[TABS.length - 1];
+
+  TABS.forEach((element) => {
+    element.addEventListener("keydown", function (e) {
+      if ((e.keyCode || e.which) === 38 || (e.keyCode || e.which) === 37) {
+        if (element == firstTab) {
+          e.preventDefault();
+          lastTab.focus();
+        } else {
+          e.preventDefault();
+          const focusableElement = TABS.indexOf(element) - 1;
+          TABS[focusableElement].focus();
+        }
+      } else if (
+        (e.keyCode || e.which) === 40 ||
+        (e.keyCode || e.which) === 39
+      ) {
+        if (element == lastTab) {
+          e.preventDefault();
+          firstTab.focus();
+        } else {
+          e.preventDefault();
+          const focusableElement = TABS.indexOf(element) + 1;
+          TABS[focusableElement].focus();
+        }
+      }
+    });
+  });
+};
+
+const determineTabindex = () => {
+  TABPANELS.forEach((element) => {
+    const focusableElements = element.querySelectorAll(
+      'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'
+    ).length;
+
+    focusableElements
+      ? element.setAttribute("tabindex", "-1")
+      : element.setAttribute("tabindex", "0");
+  });
+};
+
+const handleClick = () => {
+  TABS.forEach((element) => {
+    element.addEventListener("click", function () {
+      setSelectedTab(element);
+      showActivePanel(element);
+    });
+  });
+
+  TABS.forEach((element) => {
+    element.addEventListener("keydown", function (e) {
+      if ((e.keyCode || e.which) === 32) {
+        setSelectedTab(element);
+        showActivePanel(element);
+        element.click();
+      }
+    });
+  });
+};
+
+const activateFirstPanel = () => {
+  TABS[0].setAttribute("tabindex", "0");
+  TABS[0].setAttribute("aria-selected", "true");
+  TABPANELS[0].classList.add("visible");
+};
+
+const checkInitialSelectedTab = () => {
+  const targetedTabPanel = document
+    .querySelector(".tabpanel:target")
+    .getAttribute("aria-labelledby");
+  const selectedTab = document.querySelector(`#${targetedTabPanel}`);
+  selectedTab.setAttribute("aria-selected", "true");
+  selectedTab.removeAttribute("tabindex");
+};
+
+const handleInitialState = () => {
+  TABS.forEach((e) => {
+    e.setAttribute("tabindex", "-1");
+    e.setAttribute("aria-selected", "false");
+  });
+
+  window.location.href.indexOf("#panel") === -1
+    ? activateFirstPanel()
+    : checkInitialSelectedTab();
+
+  determineTabindex();
+};
+
+handleClick();
+createArrowNavigation();
+
+window.onload = handleInitialState;
